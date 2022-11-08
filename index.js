065bd97cd6c67c1866383110a00fdb20c9ef5ecc
BradleyGCF <bradleycarucci@gmail.com>
2022-11-08 17:39:35 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+const port = process.env.PORT || 8080;
+
+// ---------------RUTAS------------------------
+
+const about = require("./JSON/About.json");
+const portafolio = require("./JSON/Portafolio.json");
+
+app.get('/about', (req, res) => {
+    res.json(about)
+});
+
+app.get('/portafolio', (req, res) => {
+    res.json(portafolio)
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server listening on ${port}`);
+});
