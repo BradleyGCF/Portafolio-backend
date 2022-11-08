ce722aae617c4cb70c9fe08aefe79a37ce001941
BradleyGCF <carucib@gmail.com>
2022-11-08 18:41:13 -0400

    Agrego ruta /

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const port = process.env.PORT || 8080;
 const about = require("./JSON/About.json");
 const portafolio = require("./JSON/Portafolio.json");
 
+app.get("/", (req, res) => {
+    res.send("Amigo functions")
+})
 app.get('/about', (req, res) => {
     res.json(about)
 });
